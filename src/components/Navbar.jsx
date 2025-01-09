435e8597919ba43671d2b8a798ef5fb3d33b9a85
BhartiBhoyare <bhartibhoyare@gmail.com>
2025-01-09 23:42:21 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,18 @@
 import React from "react";
+import logo from "../assets/logo.png";
 import upload from "../assets/upload.png";
 import user from "../assets/user.png";
-import newLogo from "../assets/newLogo.png"
 
 const Navbar = () => {
   return (
     <>
-      <nav className="flex fixed items-center lg:py-[1.1rem] py-6 px-10 w-full lg:px-7 top-0 z-50 bg-white">
-        <img className="lg:h-7 h-14 cursor-pointer" src={newLogo} alt="" />
-        <div className="hidden lg:flex font-semibold text-2xl ml-2">3D Warehouse</div>
-        <button className="hidden lg:flex font-bold bg-[#0063a3] text-white py-[0.1rem] px-4 rounded-sm ml-6">
+      <nav className="flex fixed top-0 z-50 bg-white items-center py-4 px-7">
+        <img className="h-7" src={logo} alt="" />
+        <button className="font-bold bg-[#0063a3] text-white py-[0.1rem] px-4 rounded-sm ml-6">
           Get SketchUp
         </button>
-        <img className="hidden lg:flex cursor-pointer size-4 lg:ml-[48.8rem]" src={upload} alt="" />
-        <img className="cursor-pointer size-10 ml-[35rem] lg:size-5 lg:ml-6" src={user} alt="" />
+        <img className="size-4 ml-[49.6rem]" src={upload} alt="" />
+        <img className="size-5 ml-6" src={user} alt="" />
       </nav>
     </>
   );
