5d9e8d4905be83f784317fcee654600968bf20c2
BhartiBhoyare <bhartibhoyare@gmail.com>
2025-01-12 21:41:01 +0530

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <nav className="flex fixed w-full top-0 z-50 bg-white items-center py-4 px-7 shadow-sm shadow-[#e1e2e8]">
         <img className="h-7" src={logo} alt="" />
         <div className="hidden lg:flex font-semibold text-2xl ml-3">3D Warehouse</div>
-        <button className="hidden lg:flex font-bold bg-[#0063a3] text-white py-[0.1rem] px-4 rounded-sm ml-6">
+        <button className="hidden lg:flex font-bold bg-[#0063a3] text-white py-[0.1rem] px-4 rounded-sm ml-6 hover:bg-[#217cbb]">
           Get SketchUp
         </button>
         <img className="hidden lg:flex size-4 lg:ml-[48rem]" src={upload} alt="" />
